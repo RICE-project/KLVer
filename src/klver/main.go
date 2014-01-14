8a260f132a86e8566cdf6a534f730d3102321cb2
TonyChyi <tonychee1989@gmail.com>
2014-01-14 14:19:11 +0800

    klver.go

--- src/klver/main.go
+++ src/klver/main.go
@@ -1,3 +1,24 @@
+/*
+KLVer (pronounces 'clever') is a web-based keepalived configuration tool 
+developed with go programming language. It allows a user create/delete 
+LVS instance, add/remove MASTER/BACKUP server, add/remove virtual server 
+and realserver, and build configuration file to deploy them automatically.
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation; either version 3, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program; if not, write to the 
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 package main
 
 import(
